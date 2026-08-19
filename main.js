d741270e3214789172d5ce4d5910d57c8b8da273
Clarafm10 <clara.moreschi@escola.pr.gov.br>
2026-08-19 16:21:10 -0300

    19/08

--- main.js
+++ main.js
@@ -5,19 +5,37 @@ numeroSenha.textConent=tamanhoSenha;
 const botoes=document.querySectorAll('parametro__botao');
 
 botoes[0].onclick=diminuiTamanho;
+botoes[1].onclick=aumentaTamanho;
+
 function diminuiTamanho() {
 if(tamanhoSenha>1){
-    tamanhoSenha=tamanhoSenha-1;
+    //tamanhoSenha=tamanhoSenha-1;
+    tamanhoSenha--;
 }
     
     numeroSenha.textConent=tamanhoSenha;
 }
 
-botoes[1].onclick=aumentaTamanho;
+
 function auementaTamanho() {
 if(tamanhoSenha<20){
-tamanhoSenha=tamanhoSenha+1;
+//tamanhoSenha=tamanhoSenha+1;
+tamanhoSenha++;
 }
     
     numeroSenha.textConent=tamanhoSenha;
+}
+const campoSenha=document.querySectorAll(#campo-senha);
+const letrasMaiusculas='ABCDEFG';
+geraSenha();
+function geraSenha(){
+
+for( let i = 0;i < tamanhoSenha; i++){
+    
+let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+    numeroAleatorio=Math.floor(numeroAleatorio);
+    console.log(letrasMaiusculas[numeroAleatorio]);
+}
+
+    
 }
